a36822c74fe100af531ea74c86dde01578ec84b7
penguinzarecool <151074650+penguinzarecool@users.noreply.github.com>
2026-02-16 23:05:54 -0700

    Add files via upload

--- uri-bt/src/gatt.c
+++ uri-bt/src/gatt.c
@@ -16,21 +16,18 @@ static DBusConnection *conn;
 #define CHAR_PATH    "/uri/gatt/service0/char0"
 
 /* -------------------------------------------------- */
-/* ObjectManager (application root)                   */
+/* ObjectManager                                      */
 /* -------------------------------------------------- */
 
-static DBusHandlerResult app_handler(DBusConnection *connection,
-                                     DBusMessage *message,
-                                     void *user_data)
+static DBusHandlerResult app_handler(DBusConnection *c, DBusMessage *m, void *u)
 {
-    printf("GetManagedObjects called by BlueZ\n");
     if (!dbus_message_is_method_call(
-            message,
-            "org.freedesktop.DBus.ObjectManager",
-            "GetManagedObjects"))
+            m, "org.freedesktop.DBus.ObjectManager", "GetManagedObjects"))
         return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 
-    DBusMessage *reply = dbus_message_new_method_return(message);
+    printf("GetManagedObjects called by BlueZ\n");
+
+    DBusMessage *reply = dbus_message_new_method_return(m);
     DBusMessageIter iter, objects;
 
     dbus_message_iter_init_append(reply, &iter);
@@ -39,33 +36,30 @@ static DBusHandlerResult app_handler(DBusConnection *connection,
 
     DBusMessageIter obj, ifaces, iface, props, prop, var;
 
-    /* ---------- Service ---------- */
-    dbus_message_iter_open_container(&objects, DBUS_TYPE_DICT_ENTRY, NULL, &obj);
+    /* -------- Service -------- */
     const char *svc_path = SERVICE_PATH;
-    dbus_message_iter_append_basic(&obj, DBUS_TYPE_OBJECT_PATH, &svc_path);
+    const char *svc_iface = "org.bluez.GattService1";
+    const char *uuid_key = "UUID";
+    const char *prim_key = "Primary";
+    dbus_bool_t primary = TRUE;
 
+    dbus_message_iter_open_container(&objects, DBUS_TYPE_DICT_ENTRY, NULL, &obj);
+    dbus_message_iter_append_basic(&obj, DBUS_TYPE_OBJECT_PATH, &svc_path);
     dbus_message_iter_open_container(&obj, DBUS_TYPE_ARRAY, "{sa{sv}}", &ifaces);
     dbus_message_iter_open_container(&ifaces, DBUS_TYPE_DICT_ENTRY, NULL, &iface);
-
-    const char *svc_iface = "org.bluez.GattService1";
     dbus_message_iter_append_basic(&iface, DBUS_TYPE_STRING, &svc_iface);
     dbus_message_iter_open_container(&iface, DBUS_TYPE_ARRAY, "{sv}", &props);
 
-    /* UUID */
     dbus_message_iter_open_container(&props, DBUS_TYPE_DICT_ENTRY, NULL, &prop);
-    const char *key = "UUID";
-    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &key);
+    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &uuid_key);
     dbus_message_iter_open_container(&prop, DBUS_TYPE_VARIANT, "s", &var);
-    const char *service_uuid = SERVICE_UUID;
-    dbus_message_iter_append_basic(&var, DBUS_TYPE_STRING, &service_uuid);
+    const char *svc_uuid = SERVICE_UUID;
+    dbus_message_iter_append_basic(&var, DBUS_TYPE_STRING, &svc_uuid);
     dbus_message_iter_close_container(&prop, &var);
     dbus_message_iter_close_container(&props, &prop);
 
-    /* Primary */
-    dbus_bool_t primary = TRUE;
     dbus_message_iter_open_container(&props, DBUS_TYPE_DICT_ENTRY, NULL, &prop);
-    const char *prim = "PRIMARY";
-    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &prim);
+    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &prim_key);
     dbus_message_iter_open_container(&prop, DBUS_TYPE_VARIANT, "b", &var);
     dbus_message_iter_append_basic(&var, DBUS_TYPE_BOOLEAN, &primary);
     dbus_message_iter_close_container(&prop, &var);
@@ -76,45 +70,40 @@ static DBusHandlerResult app_handler(DBusConnection *connection,
     dbus_message_iter_close_container(&obj, &ifaces);
     dbus_message_iter_close_container(&objects, &obj);
 
-    /* ---------- Characteristic ---------- */
-    dbus_message_iter_open_container(&objects, DBUS_TYPE_DICT_ENTRY, NULL, &obj);
+    /* -------- Characteristic -------- */
     const char *char_path = CHAR_PATH;
-    dbus_message_iter_append_basic(&obj, DBUS_TYPE_OBJECT_PATH, &char_path);
+    const char *char_iface = "org.bluez.GattCharacteristic1";
+    const char *svc_key = "Service";
+    const char *flags_key = "Flags";
+    const char *flag = "read";
 
+    dbus_message_iter_open_container(&objects, DBUS_TYPE_DICT_ENTRY, NULL, &obj);
+    dbus_message_iter_append_basic(&obj, DBUS_TYPE_OBJECT_PATH, &char_path);
     dbus_message_iter_open_container(&obj, DBUS_TYPE_ARRAY, "{sa{sv}}", &ifaces);
     dbus_message_iter_open_container(&ifaces, DBUS_TYPE_DICT_ENTRY, NULL, &iface);
-
-    const char *char_iface = "org.bluez.GattCharacteristic1";
     dbus_message_iter_append_basic(&iface, DBUS_TYPE_STRING, &char_iface);
     dbus_message_iter_open_container(&iface, DBUS_TYPE_ARRAY, "{sv}", &props);
 
-    /* UUID */
     dbus_message_iter_open_container(&props, DBUS_TYPE_DICT_ENTRY, NULL, &prop);
-    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &key);
+    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &uuid_key);
     dbus_message_iter_open_container(&prop, DBUS_TYPE_VARIANT, "s", &var);
     const char *char_uuid = CHAR_UUID;
     dbus_message_iter_append_basic(&var, DBUS_TYPE_STRING, &char_uuid);
     dbus_message_iter_close_container(&prop, &var);
     dbus_message_iter_close_container(&props, &prop);
 
-    /* Service */
     dbus_message_iter_open_container(&props, DBUS_TYPE_DICT_ENTRY, NULL, &prop);
-    const char *ser = "Service";
-    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &ser);
+    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &svc_key);
     dbus_message_iter_open_container(&prop, DBUS_TYPE_VARIANT, "o", &var);
-    const char *service_path = SERVICE_PATH;
-    dbus_message_iter_append_basic(&var, DBUS_TYPE_OBJECT_PATH, &service_path);
+    dbus_message_iter_append_basic(&var, DBUS_TYPE_OBJECT_PATH, &svc_path);
     dbus_message_iter_close_container(&prop, &var);
     dbus_message_iter_close_container(&props, &prop);
 
-    /* Flags */
     dbus_message_iter_open_container(&props, DBUS_TYPE_DICT_ENTRY, NULL, &prop);
-    const char *fla = "Flags";
-    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &fla);
+    dbus_message_iter_append_basic(&prop, DBUS_TYPE_STRING, &flags_key);
     dbus_message_iter_open_container(&prop, DBUS_TYPE_VARIANT, "as", &var);
     DBusMessageIter arr;
     dbus_message_iter_open_container(&var, DBUS_TYPE_ARRAY, "s", &arr);
-    const char *flag = "read";
     dbus_message_iter_append_basic(&arr, DBUS_TYPE_STRING, &flag);
     dbus_message_iter_close_container(&var, &arr);
     dbus_message_iter_close_container(&prop, &var);
@@ -126,26 +115,22 @@ static DBusHandlerResult app_handler(DBusConnection *connection,
     dbus_message_iter_close_container(&objects, &obj);
 
     dbus_message_iter_close_container(&iter, &objects);
-
-    dbus_connection_send(connection, reply, NULL);
+    dbus_connection_send(c, reply, NULL);
     dbus_message_unref(reply);
+
     return DBUS_HANDLER_RESULT_HANDLED;
 }
 
 /* -------------------------------------------------- */
-/* Characteristic methods                             */
+/* Characteristic                                     */
 /* -------------------------------------------------- */
 
-static DBusHandlerResult char_handler(DBusConnection *connection,
-                                      DBusMessage *message,
-                                      void *user_data)
+static DBusHandlerResult char_handler(DBusConnection *c, DBusMessage *m, void *u)
 {
     if (dbus_message_is_method_call(
-            message,
-            "org.bluez.GattCharacteristic1",
-            "ReadValue")) {
+            m, "org.bluez.GattCharacteristic1", "ReadValue")) {
 
-        DBusMessage *reply = dbus_message_new_method_return(message);
+        DBusMessage *reply = dbus_message_new_method_return(m);
         DBusMessageIter iter, array;
         const char *value = "URI BLE OK";
 
@@ -156,7 +141,7 @@ static DBusHandlerResult char_handler(DBusConnection *connection,
             dbus_message_iter_append_basic(&array, DBUS_TYPE_BYTE, &value[i]);
 
         dbus_message_iter_close_container(&iter, &array);
-        dbus_connection_send(connection, reply, NULL);
+        dbus_connection_send(c, reply, NULL);
         dbus_message_unref(reply);
         return DBUS_HANDLER_RESULT_HANDLED;
     }
@@ -178,32 +163,20 @@ bool gatt_init(void)
 
     dbus_connection_setup_with_g_main(conn, NULL);
 
-    static DBusObjectPathVTable app_vtable = {
-        .message_function = app_handler
-    };
-    static DBusObjectPathVTable char_vtable = {
-        .message_function = char_handler
-    };
+    static DBusObjectPathVTable app_vtable = { .message_function = app_handler };
+    static DBusObjectPathVTable char_vtable = { .message_function = char_handler };
 
     dbus_connection_register_object_path(conn, APP_PATH, &app_vtable, NULL);
-    dbus_connection_register_object_path(conn, SERVICE_PATH, &app_vtable, NULL);
     dbus_connection_register_object_path(conn, CHAR_PATH, &char_vtable, NULL);
 
     DBusMessage *msg = dbus_message_new_method_call(
-        "org.bluez",
-        "/org/bluez/hci0",
-        "org.bluez.GattManager1",
-        "RegisterApplication");
+        "org.bluez", "/org/bluez/hci0",
+        "org.bluez.GattManager1", "RegisterApplication");
 
     DBusMessageIter args, opts;
     dbus_message_iter_init_append(msg, &args);
-    //dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT_PATH, &(const char *){APP_PATH});
     const char *app_path = APP_PATH;
-    dbus_message_iter_append_basic(
-        &args,
-        DBUS_TYPE_OBJECT_PATH,
-        &app_path
-    );
+    dbus_message_iter_append_basic(&args, DBUS_TYPE_OBJECT_PATH, &app_path);
     dbus_message_iter_open_container(&args, DBUS_TYPE_ARRAY, "{sv}", &opts);
     dbus_message_iter_close_container(&args, &opts);
 
