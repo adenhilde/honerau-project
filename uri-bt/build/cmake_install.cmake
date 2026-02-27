1ac2026f11e13be1f50b8e9d0f1ae95505594d10
penguinzarecool <151074650+penguinzarecool@users.noreply.github.com>
2026-02-27 12:37:08 -0700

    Update cmake_install.cmake
    fix build path

--- uri-bt/build/cmake_install.cmake
+++ uri-bt/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/yocto-sdk/build-ls1088ardb-pb/uri-bt
+# Install script for directory: /home/yocto-sdk/build_ls1088ardb-pb/uri-bt
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/yocto-sdk/build-ls1088ardb-pb/uri-bt/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/yocto-sdk/build_ls1088ardb-pb/uri-bt/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
